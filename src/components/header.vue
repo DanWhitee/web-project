a48483c3b1848c703f59fdf9875112553fd83749
Ginoice Team <team@ginoice.com>
2021-11-06 20:36:15 +0300

    layout__Home

---
+++ src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+   <header class="hedaer">
+      <div class="container">
+         
+      </div>
+   </header>
+</template>
+
+<script>
+export default {
+   name: "Header"
+}
+</script>
+
+<style>
+
+</style>
